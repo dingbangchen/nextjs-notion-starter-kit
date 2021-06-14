c8ae2b6956a13796de95f7a394840009a1bb622e
dingbangchen <38015108+dingbangchen@users.noreply.github.com>
2021-06-14 17:26:49 -0400

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'b07f0a7305674b6186ff7a1c2243a2a1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'FredChen',
+  domain: 'https://www.notion.so/966a10c2b43e4d058836c7a5c808de16?v=b07f0a7305674b6186ff7a1c2243a2a1',
+  author: 'Fred Chen',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,9 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: 'dingbangchen',
+  linkedin: 'dingbang-chen-1031',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
